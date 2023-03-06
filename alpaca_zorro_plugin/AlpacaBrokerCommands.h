28f8eb9d9633ccf79ca8ed338a3dfff310781a2b
kzhdev <kzhdev@gmail.com>
2023-03-05 21:03:19 -0600

    add missing header

---
+++ alpaca_zorro_plugin/AlpacaBrokerCommands.h
@@ -0,0 +1,20 @@
+#pragma once
+
+//////////////////////////////////////////////////////////////////////
+// 
+// Alpaca Plugin Custom BrokerCommand Definitions
+//
+//////////////////////////////////////////////////////////////////////
+
+#define ORDERTYPE_IOC 1
+#define ORDERTYPE_GTC 2
+#define ORDERTYPE_FOK 3
+#define ORDERTYPE_DAY 4
+#define ORDERTYPE_OPG 5
+#define ORDERTYPE_CLS 6
+
+#define CREATE_ASSETLIST 2001
+#define IS_ASSET_FRACTIONABLE 2002
+#define IS_ASSET_SHORTABLE 2003
+#define IS_ASSET_EASY_TO_BORROW 2004
+#define IS_ASSET_MARGINABLE 2005
